16be75aa1cbed44c7f099881730e61e8cf0b5327
Rivlen <x.stawecki@gmail.com>
2023-12-08 14:01:23 +0100

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -10,7 +10,6 @@ try:
     create_database_query = f'CREATE DATABASE workshop2'
     cursor.execute(create_database_query)
     print(f"Database 'workshop2' created successfully")
-
     # Closing the connection
     cursor.close()
     con.close()
@@ -33,18 +32,58 @@ except OperationalError as err:
     print('Connection error')
     raise ValueError(f'Connection error: {err}')
 
-query_create_table_user = sql.SQL("""
-    CREATE TABLE {table_name} (
+query_create_table_users = sql.SQL("""
+    CREATE TABLE IF NOT EXISTS {table_name} (
         id SERIAL,
-        username VARCHAR(255) NOT NULL,
-        hashed_password VARCHAR(80),
+        name VARCHAR(50) NOT NULL,
+        email VARCHAR(120) UNIQUE,
         password VARCHAR(60) DEFAULT 'ala',
         PRIMARY KEY (id)
     )
-""").format(table_name=sql.Identifier('user'))
+""").format(table_name=sql.Identifier('users'))
 
 with cnx:
     try:
-        cursor.execute(query_create_table_user)
+        cursor.execute(query_create_table_users)
     except DatabaseError as err:
         print(err)
+
+query_create_table_message = sql.SQL("""
+    CREATE TABLE {table_name} (
+        id SERIAL,
+        from_id VARCHAR(255) NOT NULL
+    )
+""").format(table_name=sql.Identifier('test1234'))
+
+# with cnx:
+#     try:
+#         cursor.execute(query_create_table_message)
+#     except DatabaseError as err:
+#         print(err)
+
+query_select = sql.SQL("""
+    SELECT *
+    FROM user
+""").format(table_name=sql.Identifier('user'))
+
+query_insert = sql.SQL("""
+    INSERT INTO user (username, hashed_password)
+    VALUES ('Jarosław', 'Kowalski');
+""").format(table_name=sql.Identifier('user'))
+
+# with cnx:
+#     try:
+#         cursor.execute(query_select)
+#         print(cursor.fetchall())
+#     except DatabaseError as err:
+#         print(err)
+
+query_drop_user = sql.SQL("""
+    DROP TABLE user
+""").format(table_name=sql.Identifier('user'))
+
+# with cnx:
+#     try:
+#         cursor.execute(query_drop_user)
+#     except DatabaseError as err:
+#         print(err)
