91ad64fd15f6fcb20dbef76b27cd6a08af151592
Rivlen <x.stawecki@gmail.com>
2023-12-10 17:21:39 +0100

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -95,21 +95,3 @@ def query_select_tb(db_connector):
                 print(row)
     except OperationalError as err:
         print(err)
-
-# db = 'workshop2'
-# connector = {
-#     'user': 'postgres',
-#     'password': 'coderslab',
-#     'host': 'localhost',
-#     'port': '5433'
-# }
-#
-# create_db(connector, db)
-#
-# connector['database'] = db
-#
-# query_create_tb_users(connector)
-#
-# # query_insert_into_tb(connector)
-#
-# query_select_tb(connector)
